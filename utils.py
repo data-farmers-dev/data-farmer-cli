250103cdd33e5fd97d4df7334af531258a734bf2
Mateusz Benecki <benecki.mateusz@gmail.com>
2022-05-07 15:02:28 +0200

    Add a missing return type

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ Various utilities used throughout the application.
 import docker
 
 
-def get_docker_client(base_url: str):
+def get_docker_client(base_url: str) -> docker.DockerClient:
     """
     :param base_url: URL to the Docker server
     :return: A DockerClient instance
